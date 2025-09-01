843a1f5ba72374b962e81e935ed6e41cae636331
davidvenialgo <davidismael.dv2@gmail.com>
2025-09-01 17:13:35 -0300

    Modificando config

--- public/config.js
+++ public/config.js
@@ -1,19 +1,19 @@
-// Configuración de la aplicación en localhost
-//const API_BASE_URL = 'http://127.0.0.1:8000/api';
-//const API_URL = `${API_BASE_URL}/tareas`;
-//const LOGIN_URL = `${API_BASE_URL}/login`;
-//const USUARIO_URL = `${API_BASE_URL}/usuario`;
-//const USUARIOS_URL = `${API_BASE_URL}/usuarios`;
-//const TAREAS_TODAS_URL = `${API_BASE_URL}/tareas-todas`;
-//const REGISTER_URL = `${API_BASE_URL}/register`;
-//const STORAGE_URL = 'http://127.0.0.1:8000';
+// Configuración de la aplicación para producción
+const API_BASE_URL = 'https://gestor-tareas-dv.mnz.dom.my.id/api';
+const API_URL = `${API_BASE_URL}/tareas`;
+const LOGIN_URL = `${API_BASE_URL}/login`;
+const USUARIO_URL = `${API_BASE_URL}/usuario`;
+const USUARIOS_URL = `${API_BASE_URL}/usuarios`;
+const TAREAS_TODAS_URL = `${API_BASE_URL}/tareas-todas`;
+const REGISTER_URL = `${API_BASE_URL}/register`;
+const STORAGE_URL = 'https://gestor-tareas-dv.mnz.dom.my.id';
 
-// URLs para producción (descomenta si es necesario)
- const API_BASE_URL = 'https://gestor-tareas-dv.mnz.dom.my.id/api';
- const API_URL = `${API_BASE_URL}/tareas`;
- const LOGIN_URL = `${API_BASE_URL}/login`;
- const USUARIO_URL = `${API_BASE_URL}/usuario`;
- const USUARIOS_URL = `${API_BASE_URL}/usuarios`;
- const TAREAS_TODAS_URL = `${API_BASE_URL}/tareas-todas`;
- const REGISTER_URL = `${API_BASE_URL}/register`;
- const STORAGE_URL = 'https://gestor-tareas-dv.mnz.dom.my.id';
+// Para desarrollo local, descomenta las siguientes líneas y comenta las de arriba:
+// const API_BASE_URL = 'http://127.0.0.1:8000/api';
+// const API_URL = `${API_BASE_URL}/tareas`;
+// const LOGIN_URL = `${API_BASE_URL}/login`;
+// const USUARIO_URL = `${API_BASE_URL}/usuario`;
+// const USUARIOS_URL = `${API_BASE_URL}/usuarios`;
+// const TAREAS_TODAS_URL = `${API_BASE_URL}/tareas-todas`;
+// const REGISTER_URL = `${API_BASE_URL}/register`;
+// const STORAGE_URL = 'http://127.0.0.1:8000';
